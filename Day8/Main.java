06a5788d62d5415d867a4668c2f0e89a641794ff
thisizharini <nithyaharini2625@gmail.com>
2025-10-31 07:09:03 +0530

    Day8

---
+++ Day8/Main.java
@@ -0,0 +1,21 @@
+//Sum of each digits of the number
+package Day8;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=scanner.nextInt();
+        int sum=0;
+        while(num>0){
+            
+            sum=sum+ num%10;//sum+=i
+            num=num/10;
+
+        }
+        System.out.println(sum);
+
+        scanner.close();
+    }}
+
