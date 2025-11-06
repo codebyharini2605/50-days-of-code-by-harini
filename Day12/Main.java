0462e961bcb40a9a6bb91d095e0907cc6de1c92e
thisizharini <nithyaharini2625@gmail.com>
2025-11-06 22:05:28 +0530

    Day12

---
+++ Day12/Main.java
@@ -0,0 +1,18 @@
+package Day12;
+
+public class Main {
+    public static void main(String[] args) {
+        String name="HARINI";
+        name=name.toLowerCase();
+        int count=0;
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i)=='a' || name.charAt(i)=='e' || name.charAt(i)=='i' || name.charAt(i)=='o' || name.charAt(i)=='u'){
+                count++;
+            }
+            
+        }
+        System.out.println("THE TOTAL NUMBER OF VOWELS IN THIS NAME IS " + count);
+
+    }
+    
+}
